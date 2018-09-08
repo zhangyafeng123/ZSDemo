33df14707b10e2928758b14756c05f633b4f7b2c
张亚峰 <1240334911@qq.com>
2018-09-08 22:59:22 +0800

    初始化项目

---
+++ MyTestProject/MyTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTestProject
+//
+//  Created by 张亚峰 on 2018/9/8.
+//  Copyright © 2018年 zhangyafeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTestProject/MyTestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTestProject
+//
+//  Created by 张亚峰 on 2018/9/8.
+//  Copyright © 2018年 zhangyafeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
