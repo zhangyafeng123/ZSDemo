84f9bef2d5fc2fb9c79c6e9c4e15172a1a59b216
张亚峰 <1240334911@qq.com>
2018-09-08 23:03:11 +0800

    框架开发完成

---
+++ MyTestProject/我的框架/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  MyTestProject
+//
+//  Created by 张亚峰 on 2018/9/8.
+//  Copyright © 2018年 zhangyafeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (nonatomic, copy)NSString *name;
+
+- (void)eat;
+
+@end
---
+++ MyTestProject/我的框架/Person.m
@@ -0,0 +1,18 @@
+//
+//  Person.m
+//  MyTestProject
+//
+//  Created by 张亚峰 on 2018/9/8.
+//  Copyright © 2018年 zhangyafeng. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+-(void)eat
+{
+    NSLog(@"%@ 正在吃",_name);
+}
+
+@end
